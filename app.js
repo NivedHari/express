1ef852aa33406c04eeb6e68bda0775fea6e5b41f
NivedHari <nivedhari44@gmail.com>
2024-02-16 12:32:18 +0530

    Lets get started with Express

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('in The middleware');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('in another middleware');
+    res.send({ key1: 'value' });
+});
+
+app.listen(3000);
